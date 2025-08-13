a442994e1199d33ec466a76a5e430395785d0704
muhammed-udaif <muhammedudaif07@gamil.com>
2025-08-13 14:58:05 +0530

    .

--- src/Componets/CourseCoupon.jsx
+++ src/Componets/CourseCoupon.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import './CourseCoupon.css'
 import borderTick from '../assests/border-tick.svg'
+import heart from '../assests/heart.svg'
 
 function CourseCoupon() {
+    const shareCoupon = [
+        {head:"Share"},
+        {head:"Gift this course"},
+        {head:"Apply coupon"}
+    ]
   return (
     <>
     <div className="course-coupon-container">
@@ -47,6 +53,60 @@ function CourseCoupon() {
                                 </div>
                             </div>
                             <div className="or-container">or</div>
+                            <div className="cart-container">
+                                <div className="price-cart-container">
+                                    <div className="price-container">
+                                        <div className="inside-price-container">
+                                            <div className="price-item">₹549</div>
+                                        </div>
+                                    </div>
+                                    <div className="add-cart-container">
+                                        <div className="add-to-cart">
+                                            <button className='add-cart-btn inside-start-subscription'>Add to cart</button>
+                                        </div>
+                                        <div>
+                                            <button className="heart inside-start-subscription">
+                                                <img src={heart} alt="heart-img" className='heart-img' />
+                                            </button>
+                                        </div>
+                                    </div>
+                                    <div className="buy-now-container ">
+                                        <button className='buy-now-btn inside-start-subscription'>Buy now</button>
+                                    </div>
+                                </div>
+                                <div className="money-back-container">
+                                    <div className="inside-money-back-container">
+                                        <span className='money-back-days'>30-Day Money-Back Guarantee</span>
+                                    </div>
+                                </div>
+                                <div className="share-gift-coupon-container">
+                                    <div className="inside-share-gift-coupon">
+                                        {shareCoupon.map((item,index) => (
+                                             <button className='share' key={index}>{item.head}</button>
+                                        ))}
+                                    </div>
+                                    <div>
+                                        <div className="redeem-straight"></div>
+                                        <div className="redeemed-coupon">
+                                            <div className="inside-redeemed-coupon">
+                                                <div className="inside-udemy-coupon">
+                                                    <ul>
+                                                        <li>
+                                                            <b>MT130825A</b>
+                                                             is applied
+                                                        </li>
+                                                        <li>Udemy coupon</li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <form  className="enter-coupon-container">
+                                            <input type="text" placeholder='Enter Coupon' className='input-coupon' />
+                                            <button className='apply-coupon inside-start-subscription'>Apply</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
